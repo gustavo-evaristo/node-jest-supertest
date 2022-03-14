417a112e10d5d1240bdc4948d514c27a752593a6
Gustavo Evaristo <gug.henri1@gmail.com>
2022-03-14 19:03:11 -0300

    fix

--- jest.config.ts
+++ jest.config.ts
@@ -26,9 +26,10 @@ export default {
   // coverageDirectory: undefined,
 
   // An array of regexp pattern strings used to skip coverage collection
-  // coveragePathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "/migrations/",
+  ],
 
   // Indicates which provider should be used to instrument code for coverage
   coverageProvider: "v8",
